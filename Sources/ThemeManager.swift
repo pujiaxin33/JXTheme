7323c42bcdfbe5f86cb94374fab78997eb0b5123
pujiaxin33 <317437084@qq.com>
2019-07-10 17:45:56 +0800

    添加JXThemeDidChange

--- Sources/ThemeManager.swift
+++ Sources/ThemeManager.swift
@@ -8,6 +8,10 @@
 
 import Foundation
 
+extension Notification.Name {
+    public static let JXThemeDidChange = Notification.Name("com.jiaxin.theme.themeDidChangeNotification")
+}
+
 public class ThemeManager {
     public static let shared = ThemeManager()
     public private(set) var currentThemeStyle: ThemeStyle = .light
@@ -22,6 +26,7 @@ public class ThemeManager {
     //TODO:userdefaults存储themestyle
     public func themeStyleDidChange(_ style: ThemeStyle) {
         currentThemeStyle = style
+        NotificationCenter.default.post(name: NSNotification.Name.JXThemeDidChange, object: nil, userInfo: ["style" : style])
         DispatchQueue.main.async {
             self.viewsHashTable.allObjects.forEach { (object) in
                 //让object根据最新的style刷新
