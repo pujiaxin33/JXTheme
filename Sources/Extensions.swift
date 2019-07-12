280cdc81cc179144f7c0eb04af134c45df97f733
pujiaxin33 <317437084@qq.com>
2019-07-12 10:27:34 +0800

    添加setupLayerThemeProperty方法

--- Sources/Extensions.swift
+++ Sources/Extensions.swift
@@ -21,6 +21,18 @@ func setupViewThemeProperty<T>(view: UIView, key: String, provider: ThemePropert
         view.configs.removeValue(forKey: key)
     }
 }
+func setupLayerThemeProperty<T>(layer: CALayer, key: String, provider: ThemePropertyDynamicProvider<T>?, customization: @escaping () -> ()) {
+    if provider != nil {
+        let config: ThemeCustomizationClosure = {(style) in
+            customization()
+        }
+        layer.configs[key] = config
+        customization()
+        ThemeManager.shared.trackedHashTable.add(layer)
+    }else {
+        layer.configs.removeValue(forKey: key)
+    }
+}
 
 //MARK: - ThemeWapper
 public extension ThemeWapper where Base: UIView {
@@ -31,135 +43,85 @@ public extension ThemeWapper where Base: UIView {
                 baseItem.backgroundColor = new?(ThemeManager.shared.currentThemeStyle)
             }
         }
-        get {
-            return nil
-        }
+        get { return nil }
     }
 
     var tintColor: ThemeColorDynamicProvider? {
         set(new) {
-            if new != nil {
-                let baseItem = self.base
-                let config: ThemeCustomizationClosure = {[weak baseItem] (style) in
-                    baseItem?.tintColor = new?(style)
-                }
-                self.base.configs["UIView.tintColor"] = config
-                self.base.tintColor = new?(ThemeManager.shared.currentThemeStyle)
-                ThemeManager.shared.trackedHashTable.add(self.base)
-            }else {
-                self.base.configs.removeValue(forKey: "UIView.tintColor")
+            let baseItem = self.base
+            setupViewThemeProperty(view: self.base, key: "UIView.tintColor", provider: new) {
+                baseItem.tintColor = new?(ThemeManager.shared.currentThemeStyle)
             }
         }
-        get {
-            return nil
-        }
+        get { return nil }
     }
     var customization: ThemeCustomizationClosure? {
         set(new) {
-            if new != nil {
-                self.base.configs["UIView.customization"] = new!
+            setupViewThemeProperty(view: self.base, key: "UIView.customization", provider: new) {
                 new?(ThemeManager.shared.currentThemeStyle)
-                ThemeManager.shared.trackedHashTable.add(self.base)
-            }else {
-                self.base.configs.removeValue(forKey: "UIView.customization")
             }
         }
-        get {
-            return nil
-        }
+        get { return nil }
     }
 }
 public extension ThemeWapper where Base: UILabel {
     var textColor: ThemeColorDynamicProvider? {
         set(new) {
-            if new != nil {
-                let baseItem = self.base
-                let config: ThemeCustomizationClosure = {[weak baseItem] (style) in
-                    baseItem?.textColor = new?(style)
-                }
-                self.base.configs["UILabel.textColor"] = config
-                self.base.textColor = new?(ThemeManager.shared.currentThemeStyle)
-                ThemeManager.shared.trackedHashTable.add(self.base)
-            }else {
-                self.base.configs.removeValue(forKey: "UILabel.textColor")
+            let baseItem = self.base
+            setupViewThemeProperty(view: self.base, key: "UILabel.textColor", provider: new) {
+                baseItem.textColor = new?(ThemeManager.shared.currentThemeStyle)
             }
         }
-        get {
-            return nil
-        }
+        get { return nil }
     }
     var attributedText: ThemeAttributedTextDynamicProvider? {
         set(new) {
-            if new != nil {
-                let baseItem = self.base
-                let config: ThemeCustomizationClosure = {[weak baseItem] (style) in
-                    baseItem?.attributedText = new?(style)
-                }
-                self.base.configs["UILabel.attributedText"] = config
-                self.base.attributedText = new?(ThemeManager.shared.currentThemeStyle)
-                ThemeManager.shared.trackedHashTable.add(self.base)
-            }else {
-                self.base.configs.removeValue(forKey: "UILabel.attributedText")
+            let baseItem = self.base
+            setupViewThemeProperty(view: self.base, key: "UILabel.attributedText", provider: new) {
+                baseItem.attributedText = new?(ThemeManager.shared.currentThemeStyle)
             }
         }
-        get {
-            return nil
-        }
+        get { return nil }
     }
 }
 
 public extension ThemeWapper where Base: UIButton {
-    func setTitleColor(_ colorProvider: @escaping ThemeColorDynamicProvider, for state: UIControl.State) {
+    func setTitleColor(_ colorProvider: ThemeColorDynamicProvider?, for state: UIControl.State) {
         let baseItem = self.base
-        let config: ThemeCustomizationClosure = {[weak baseItem] (style) in
-            baseItem?.setTitleColor(colorProvider(style), for: state)
+        setupViewThemeProperty(view: self.base, key: "UIButton.titleColor.\(state.rawValue)", provider: colorProvider) {
+            baseItem.setTitleColor(colorProvider?(ThemeManager.shared.currentThemeStyle), for: state)
         }
-        self.base.configs["UIButton.titleColor.\(state.rawValue)"] = config
-        self.base.setTitleColor(colorProvider(ThemeManager.shared.currentThemeStyle), for: state)
-        ThemeManager.shared.trackedHashTable.add(self.base)
     }
 
-    func setTitleShadowColor(_ colorProvider: @escaping ThemeColorDynamicProvider, for state: UIControl.State) {
+    func setTitleShadowColor(_ colorProvider: ThemeColorDynamicProvider?, for state: UIControl.State) {
         let baseItem = self.base
-        let config: ThemeCustomizationClosure = {[weak baseItem] (style) in
-            baseItem?.setTitleShadowColor(colorProvider(style), for: state)
+        setupViewThemeProperty(view: self.base, key: "UIButton.titleShadowColor.\(state.rawValue)", provider: colorProvider) {
+            baseItem.setTitleShadowColor(colorProvider?(ThemeManager.shared.currentThemeStyle), for: state)
         }
-        self.base.configs["UIButton.titleShadowColor.\(state.rawValue)"] = config
-        self.base.setTitleShadowColor(colorProvider(ThemeManager.shared.currentThemeStyle), for: state)
-        ThemeManager.shared.trackedHashTable.add(self.base)
     }
 
-    func setAttributedTitle(_ textProvider: @escaping ThemeAttributedTextDynamicProvider, for state: UIControl.State) {
+    func setAttributedTitle(_ textProvider: ThemeAttributedTextDynamicProvider?, for state: UIControl.State) {
         let baseItem = self.base
-        let config: ThemeCustomizationClosure = {[weak baseItem] (style) in
+        setupViewThemeProperty(view: self.base, key: "UIButton.attributedTitle.\(state.rawValue)", provider: textProvider) {
             UIView.setAnimationsEnabled(false)
-            baseItem?.setAttributedTitle(textProvider(style), for: state)
-            baseItem?.layoutIfNeeded()
+            baseItem.setAttributedTitle(textProvider?(ThemeManager.shared.currentThemeStyle), for: state)
+            baseItem.layoutIfNeeded()
             UIView.setAnimationsEnabled(true)
         }
-        self.base.configs["UIButton.attributedTitle.\(state.rawValue)"] = config
-        self.base.setAttributedTitle(textProvider(ThemeManager.shared.currentThemeStyle), for: state)
-        ThemeManager.shared.trackedHashTable.add(self.base)
     }
 
-    func setImage(_ imageProvider: @escaping ThemeImageDynamicProvider,for state: UIControl.State) {
+    func setImage(_ imageProvider: ThemeImageDynamicProvider?, for state: UIControl.State) {
         let baseItem = self.base
-        let config: ThemeCustomizationClosure = {[weak baseItem] (style) in
-            baseItem?.setImage(imageProvider(style), for: state)
+        setupViewThemeProperty(view: self.base, key: "UIButton.image.\(state.rawValue)", provider: imageProvider) {
+            baseItem.setImage(imageProvider?(ThemeManager.shared.currentThemeStyle), for: state)
         }
-        self.base.configs["UIButton.image.\(state.rawValue)"] = config
-        self.base.setImage(imageProvider(ThemeManager.shared.currentThemeStyle), for: state)
-        ThemeManager.shared.trackedHashTable.add(self.base)
     }
 
-    func setBackgroundImage(_ imageProvider: @escaping ThemeImageDynamicProvider, for state: UIControl.State) {
+    func setBackgroundImage(_ imageProvider: ThemeImageDynamicProvider?, for state: UIControl.State) {
         let baseItem = self.base
-        let config: ThemeCustomizationClosure = {[weak baseItem] (style) in
-            baseItem?.setBackgroundImage(imageProvider(style), for: state)
+        setupViewThemeProperty(view: self.base, key: "UIButton.backgroundImage.\(state.rawValue)", provider: imageProvider) {
+            baseItem.setBackgroundImage(imageProvider?(ThemeManager.shared.currentThemeStyle), for: state)
         }
-        self.base.configs["UIButton.backgroundImage.\(state.rawValue)"] = config
-        self.base.setBackgroundImage(imageProvider(ThemeManager.shared.currentThemeStyle), for: state)
-        ThemeManager.shared.trackedHashTable.add(self.base)
     }
 }
 //TODO:navigationBar
@@ -167,185 +129,98 @@ public extension ThemeWapper where Base: UIButton {
 public extension ThemeWapper where Base: UITextField {
     var textColor: ThemeColorDynamicProvider? {
         set(new) {
-            if new != nil {
-                let baseItem = self.base
-                let config: ThemeCustomizationClosure = {[weak baseItem] (style) in
-                    baseItem?.textColor = new?(style)
-                }
-                self.base.configs["UITextField.textColor"] = config
-                self.base.textColor = new?(ThemeManager.shared.currentThemeStyle)
-                ThemeManager.shared.trackedHashTable.add(self.base)
-            }else {
-                self.base.configs.removeValue(forKey: "UITextField.textColor")
+            let baseItem = self.base
+            setupViewThemeProperty(view: self.base, key: "UITextField.textColor", provider: new) {
+                baseItem.textColor = new?(ThemeManager.shared.currentThemeStyle)
             }
         }
-        get {
-            return nil
-        }
+        get { return nil }
     }
     var attributedText: ThemeAttributedTextDynamicProvider? {
         set(new) {
-            if new != nil {
-                let baseItem = self.base
-                let config: ThemeCustomizationClosure = {[weak baseItem] (style) in
-                    baseItem?.attributedText = new?(style)
-                }
-                self.base.configs["UITextField.attributedText"] = config
-                self.base.attributedText = new?(ThemeManager.shared.currentThemeStyle)
-                ThemeManager.shared.trackedHashTable.add(self.base)
-            }else {
-                self.base.configs.removeValue(forKey: "UITextField.attributedText")
+            let baseItem = self.base
+            setupViewThemeProperty(view: self.base, key: "UITextField.attributedText", provider: new) {
+                baseItem.attributedText = new?(ThemeManager.shared.currentThemeStyle)
             }
         }
-        get {
-            return nil
-        }
+        get { return nil }
     }
     var attributedPlaceholder: ThemeAttributedTextDynamicProvider? {
         set(new) {
-            if new != nil {
-                let baseItem = self.base
-                let config: ThemeCustomizationClosure = {[weak baseItem] (style) in
-                    baseItem?.attributedPlaceholder = new?(style)
-                }
-                self.base.configs["UITextField.attributedPlaceholder"] = config
-                self.base.attributedPlaceholder = new?(ThemeManager.shared.currentThemeStyle)
-                ThemeManager.shared.trackedHashTable.add(self.base)
-            }else {
-                self.base.configs.removeValue(forKey: "UITextField.attributedPlaceholder")
+            let baseItem = self.base
+            setupViewThemeProperty(view: self.base, key: "UITextField.attributedPlaceholder", provider: new) {
+                baseItem.attributedPlaceholder = new?(ThemeManager.shared.currentThemeStyle)
             }
         }
-        get {
-            return nil
-        }
+        get { return nil }
     }
     var keyboardAppearance: ThemeKeyboardAppearanceDynamicProvider? {
         set(new) {
-            if new != nil {
-                let baseItem = self.base
-                let config: ThemeCustomizationClosure = {[weak baseItem] (style) in
-                    baseItem?.keyboardAppearance = new?(style) ?? .default
-                }
-                self.base.configs["UITextField.keyboardAppearance"] = config
-                self.base.keyboardAppearance = new?(ThemeManager.shared.currentThemeStyle) ?? .default
-                ThemeManager.shared.trackedHashTable.add(self.base)
-            }else {
-                self.base.configs.removeValue(forKey: "UITextField.keyboardAppearance")
+            let baseItem = self.base
+            setupViewThemeProperty(view: self.base, key: "UITextField.keyboardAppearance", provider: new) {
+                baseItem.keyboardAppearance = new?(ThemeManager.shared.currentThemeStyle) ?? .default
             }
         }
-        get {
-            return nil
-        }
+        get { return nil }
     }
 }
 public extension ThemeWapper where Base: UITextView {
     var textColor: ThemeColorDynamicProvider? {
         set(new) {
-            if new != nil {
-                let baseItem = self.base
-                let config: ThemeCustomizationClosure = {[weak baseItem] (style) in
-                    baseItem?.textColor = new?(style)
-                }
-                self.base.configs["textColor"] = config
-                self.base.textColor = new?(ThemeManager.shared.currentThemeStyle)
-                ThemeManager.shared.trackedHashTable.add(self.base)
-            }else {
-                self.base.configs.removeValue(forKey: "textColor")
+            let baseItem = self.base
+            setupViewThemeProperty(view: self.base, key: "UITextView.textColor", provider: new) {
+                baseItem.textColor = new?(ThemeManager.shared.currentThemeStyle)
             }
         }
-        get {
-            return nil
-        }
+        get { return nil }
     }
     var attributedText: ThemeAttributedTextDynamicProvider? {
         set(new) {
-            if new != nil {
-                let baseItem = self.base
-                let config: ThemeCustomizationClosure = {[weak baseItem] (style) in
-                    baseItem?.attributedText = new?(style)
-                }
-                self.base.configs["attributedText"] = config
-                self.base.attributedText = new?(ThemeManager.shared.currentThemeStyle)
-                ThemeManager.shared.trackedHashTable.add(self.base)
-            }else {
-                self.base.configs.removeValue(forKey: "attributedText")
+            let baseItem = self.base
+            setupViewThemeProperty(view: self.base, key: "UITextView.attributedText", provider: new) {
+                baseItem.attributedText = new?(ThemeManager.shared.currentThemeStyle)
             }
         }
-        get {
-            return nil
-        }
+        get { return nil }
     }
     var keyboardAppearance: ThemeKeyboardAppearanceDynamicProvider? {
         set(new) {
-            if new != nil {
-                let baseItem = self.base
-                let config: ThemeCustomizationClosure = {[weak baseItem] (style) in
-                    baseItem?.keyboardAppearance = new?(style) ?? .default
-                }
-                self.base.configs["UITextField.keyboardAppearance"] = config
-                self.base.keyboardAppearance = new?(ThemeManager.shared.currentThemeStyle) ?? .default
-                ThemeManager.shared.trackedHashTable.add(self.base)
-            }else {
-                self.base.configs.removeValue(forKey: "UITextField.keyboardAppearance")
+            let baseItem = self.base
+            setupViewThemeProperty(view: self.base, key: "UITextView.keyboardAppearance", provider: new) {
+                baseItem.keyboardAppearance = new?(ThemeManager.shared.currentThemeStyle) ?? .default
             }
         }
-        get {
-            return nil
-        }
+        get { return nil }
     }
 }
 public extension ThemeWapper where Base: UIImageView {
     var image: ThemeImageDynamicProvider? {
         set(new) {
-            if new != nil {
-                let baseItem = self.base
-                let config: ThemeCustomizationClosure = {[weak baseItem] (style) in
-                    baseItem?.image = new?(style)
-                }
-                self.base.configs["image"] = config
-                self.base.image = new?(ThemeManager.shared.currentThemeStyle)
-                ThemeManager.shared.trackedHashTable.add(self.base)
-            }else {
-                self.base.configs.removeValue(forKey: "textColor")
+            let baseItem = self.base
+            setupViewThemeProperty(view: self.base, key: "UIImageView.image", provider: new) {
+                baseItem.image = new?(ThemeManager.shared.currentThemeStyle)
             }
         }
-        get {
-            return nil
-        }
+        get { return nil }
     }
 }
 public extension ThemeWapper where Base: CALayer {
     var backgroundColor: ThemeColorDynamicProvider? {
         set(new) {
-            if new != nil {
-                let baseItem = self.base
-                let config: ThemeCustomizationClosure = {[weak baseItem] (style) in
-                    baseItem?.backgroundColor = new?(style).cgColor
-                }
-                self.base.configs["backgroundColor"] = config
-                self.base.backgroundColor = new?(ThemeManager.shared.currentThemeStyle).cgColor
-                ThemeManager.shared.trackedHashTable.add(self.base)
-            }else {
-                self.base.configs.removeValue(forKey: "backgroundColor")
+            let baseItem = self.base
+            setupLayerThemeProperty(layer: self.base, key: "CALayer.backgroundColor", provider: new) {
+                baseItem.backgroundColor = new?(ThemeManager.shared.currentThemeStyle).cgColor
             }
         }
-        get {
-            return nil
-        }
+        get { return nil }
     }
     var customization: ThemeCustomizationClosure? {
         set(new) {
-            if new != nil {
-                self.base.configs["customization"] = new!
+            setupLayerThemeProperty(layer: self.base, key: "CALayer.customization", provider: new) {
                 new?(ThemeManager.shared.currentThemeStyle)
-                ThemeManager.shared.trackedHashTable.add(self.base)
-            }else {
-                self.base.configs.removeValue(forKey: "customization")
             }
         }
-        get {
-            return nil
-        }
+        get { return nil }
     }
 }
 
